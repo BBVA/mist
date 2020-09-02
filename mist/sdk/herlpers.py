f8a42815ec686c96455440c7e178a1271285c94d
german.ramos.garcia <german.ramos.garcia@bbva.com>
2020-09-02 12:34:08 +0200

    fix: get_var not returning mapped variables

--- mist/sdk/herlpers.py
+++ mist/sdk/herlpers.py
@@ -3,8 +3,6 @@ from mist.sdk.stack import stack
 
 def get_var(var):
 
-    if var in stack[len(stack)-1]:
+    if len(stack)>0 and var in stack[len(stack)-1]:
         return stack[len(stack)-1][var]
-    if var in mapped:
-        return mapped[var]
-    return None
+    return mapped.get(var)
