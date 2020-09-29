df431d07c5b0e4a622d79c9de0ebddf13cccc8c4
cr0hn <cr0hn@cr0hn.com>
2020-09-29 11:40:39 +0200

    updated - Python maniac things

--- mist/sdk/herlpers.py
+++ mist/sdk/herlpers.py
@@ -6,7 +6,7 @@ from mist.sdk.config import config
 from mist.sdk.watchers import watchers
 
 def get_var(var):
-    if len(stack)>0 and var in stack[len(stack)-1]:
+    if stack and var in stack[len(stack)-1]:
         return stack[len(stack)-1][var]
     return db.fetch_table_as_dict(var)
 
@@ -14,13 +14,13 @@ def get_id(id):
     # print(f"get_id id={id.id} string={id.string} child={id.child}")
     if not hasattr(id, "string"):
         return get_var(id)
-    if id.string != "":
+    if id.string:
         return id.string
-    elif id.data != "":
+    elif id.data:
         return id.data
-    elif id.child != "":
+    elif id.child:
         all = get_var(id.id)
-        if isinstance(all, list):
+        if type(all) is list:
             return all[len(all)-1][id.child]
         else:
             return all[id.child]
@@ -30,10 +30,11 @@ def watchedInsert(table: str, values: List[str], *, fields=None):
     if config.debug:
         print(f"-> watchedInsert {table}")
     db.insert(table, values, fields=fields)
-    if fields==None:
+    if not fields:
         fields=db.fetch_table_headers(table)[1:]
     item = dict(zip(fields, values))
-    for _, watcher in enumerate(watchers):
+
+    for watcher in watchers:
         if watcher["var"] == table:
             stack.append({watcher["name"]: item})
             for c in watcher["commands"]:
