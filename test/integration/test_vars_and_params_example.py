df0f6e763a352e54ff7e6af702c21daf0f306e51
german.ramos.garcia <german.ramos.garcia@bbva.com>
2020-09-30 16:17:21 +0200

    test: var and params

---
+++ test/integration/test_vars_and_params_example.py
@@ -0,0 +1,15 @@
+import os
+
+from mist.interpreter import execute_from_text
+
+PING_EXAMPLE_FILE = "vars and params.mist"
+
+def test_exec_example(examples_path):
+    with open(os.path.join(examples_path, PING_EXAMPLE_FILE), "r") as f:
+        content = f.read()
+
+    console = execute_from_text(content, {"p1": "adios"})
+
+    assert "hola" in console
+    assert "/bin" in console
+    assert "adios" in console
