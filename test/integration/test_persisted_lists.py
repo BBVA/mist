bb245696f7b2f74e50b4a21c7f9112d5e10aa1af
german.ramos.garcia <german.ramos.garcia@bbva.com>
2020-10-06 16:03:15 +0200

    test: persisted lists

---
+++ test/integration/test_persisted_lists.py
@@ -0,0 +1,23 @@
+import os
+
+from mist.interpreter import execute_from_text
+
+EXAMPLE_FILE = "persisted lists.mist"
+
+def test_persisted_lists(examples_path):
+    with open(os.path.join(examples_path, EXAMPLE_FILE), "r") as f:
+        content = f.read()
+
+    console = execute_from_text(content)
+
+    assert """[{'id': 1, 'Name': 'Letters', 'Objects': ['A', 'B']}, {'id': 2, 'Name': 'Numbers', 'Objects': ['1', '2']}]
+{'id': 1, 'Name': 'Letters', 'Objects': ['A', 'B']}
+['A', 'B']
+A
+B
+{'id': 2, 'Name': 'Numbers', 'Objects': ['1', '2']}
+['1', '2']
+1
+2
+""" == console    
+
