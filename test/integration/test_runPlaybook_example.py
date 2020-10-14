a5fe205a4a7f05472e261c1f86bd3d01d70d002c
german.ramos.garcia <german.ramos.garcia@bbva.com>
2020-10-14 10:51:59 +0200

    feat: runPlaybookCommand

---
+++ test/integration/test_runPlaybook_example.py
@@ -0,0 +1,14 @@
+import os
+
+from mist.action_exec import execute_from_text
+
+EXAMPLE_FILE = "runPlaybook.mist"
+
+def test_exec_example(examples_path):
+    with open(os.path.join(examples_path, EXAMPLE_FILE), "r") as f:
+        content = f.read()
+
+    console = execute_from_text(content, {"p1": "adios"})
+
+    assert "127.0.0.1" in console
+    assert "adios" in console
