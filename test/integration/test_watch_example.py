39058531cde23daea4677799cd73309337a7c987
cr0hn <cr0hn@cr0hn.com>
2020-10-20 12:57:39 +0200

    fix - file name var

--- test/integration/test_watch_example.py
+++ test/integration/test_watch_example.py
@@ -2,10 +2,10 @@ import os
 
 from mist.action_exec import execute_from_text
 
-PING_EXAMPLE_FILE = "watch.mist"
+WATCH_FILE = "watch.mist"
 
 def test_watch_example(examples_path):
-    with open(os.path.join(examples_path, PING_EXAMPLE_FILE), "r") as f:
+    with open(os.path.join(examples_path, WATCH_FILE), "r") as f:
         content = f.read()
 
     console = execute_from_text(content)
