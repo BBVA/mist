25313144835befe20725dcdb90443863b02f4c3f
Héctor Hurtado <hector.hurtado@bbva.com>
2020-11-06 15:00:54 +0100

    Set different path for Linux and MacOS

--- test/integration/test_check.py
+++ test/integration/test_check.py
@@ -1,34 +1,44 @@
 import os
+import platform
 
 from mist.action_run import execute_from_text
 
 CHECK_FILE = "check.mist"
 CHECK_ELSE_FILE = "check_else.mist"
 
+# For 'Linux'
+paramPath = "/bin"
+
+# For MacOSX
+if (platform.system() == 'Darwin'):
+    paramPath = "/usr/bin"
+
+# Who the hell uses 'Windows'
+
 def test_check_success(examples_path):
     with open(os.path.join(examples_path, CHECK_FILE), "r") as f:
         content = f.read()
 
-    output = execute_from_text(content, {"param": "/bin/true"})
+    output = execute_from_text(content, {"param": f"{paramPath}/true"})
     assert "FOO\n" == output
 
 def test_check_fail(examples_path):
     with open(os.path.join(examples_path, CHECK_FILE), "r") as f:
         content = f.read()
 
-    output = execute_from_text(content, {"param": "/bin/false"})
+    output = execute_from_text(content, {"param": f"{paramPath}/false"})
     assert "" == output
 
 def test_check_else_branch_1(examples_path):
     with open(os.path.join(examples_path, CHECK_ELSE_FILE), "r") as f:
         content = f.read()
 
-    output = execute_from_text(content, {"param": "/bin/true"})
+    output = execute_from_text(content, {"param": f"{paramPath}/true"})
     assert "FOO\n" == output
 
 def test_check_else_branch_2(examples_path):
     with open(os.path.join(examples_path, CHECK_ELSE_FILE), "r") as f:
         content = f.read()
 
-    output = execute_from_text(content, {"param": "/bin/false"})
+    output = execute_from_text(content, {"param": f"{paramPath}/false"})
     assert "BAR\n" == output
