63b9e1c7b42f01905be0b5d314b1bdd9032e82bc
Héctor Hurtado <hector.hurtado@bbva.com>
2020-12-03 18:13:04 +0100

    Resolve merge bug in mist/sdk/__init__.py

--- mist/sdk/__init__.py
+++ mist/sdk/__init__.py
@@ -6,7 +6,6 @@ from .environment import environment
 from .herlpers import get_id, get_param, get_key, command_runner, function_runner
 from .stack import stack
 from .watchers import watchers
-from .commands import commands
 from .function import functions
 from .cmd import execution
 from .config import config
