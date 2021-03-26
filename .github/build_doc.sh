dccf8dced667b0220d425716a5e2499e826f48d4
Héctor Hurtado <hector.hurtado@bbva.com>
2021-03-26 14:52:54 +0100

    Generation of new documentation files

--- .github/build_doc.sh
+++ .github/build_doc.sh
@@ -1,78 +1,77 @@
 #! /bin/bash
 
+GENERATION_ROOT=generated_docs/content
+NAV_FILE="$GENERATION_ROOT/_data/navigation.yml"
+DOCS_FILE="$GENERATION_ROOT/_includes/docs_incl.md"
+LANG_FUNCS_FILE="$GENERATION_ROOT/_includes/funcs_incl.md"
+LANG_CATALOG_FILE="$GENERATION_ROOT/_includes/catal_incl.md"
+LANG_SYNTAX_FILE="$GENERATION_ROOT/_includes/syntax_incl.md"
+
 # Create the directory tree in which to generate the documentation files
 mkdir -vp generated_docs/content/{_data,_includes,assets/images} 2> /dev/null
 
 # copy assets
 cp -vr images/*.png generated_docs/content/assets/images
 
-
-echo -e "Generating doc files: \ngenerated_docs/content/_includes/index_incl.md"
-csplit --prefix=docs --suppress-matched  README.md '/<!--split-->/' '{*}'
-sed -e 's#https://raw.githubusercontent.com/BBVA/mist/master#content/assets#' docs00 docs02 > generated_docs/content/_includes/index_incl.md
-
-echo "generated_docs/content/_includes/intro_incl.md"
-sed -e 's/<!---/---/' -e 's/--->/---/' mist/lang/README.md > generated_docs/content/_includes/intro_incl.md
-
-echo "generated_docs/content/_includes/builtin_incl.md"
-cp -v mist/lang/BUILTIN.md generated_docs/content/_includes/builtin_incl.md
-
-echo "generated_docs/content/_includes/quick_incl.md"
-sed -e 's#https://raw.githubusercontent.com/BBVA/mist/master#content/assets#' docs01 > generated_docs/content/_includes/quick_incl.md
-
-rm docs??
-
-echo "Generating main index: generated_docs/content/_data/navigation.yml"
 # Create main index yaml
-cat <<EOF > generated_docs/content/_data/navigation.yml
+echo "Generating main index: $NAV_FILE"
+cat <<EOF > $NAV_FILE
 # main links
 main:
   - title: "Home"
     url: /
-  - title: "Introduction"
-    url: intro.html
   - title: "Quick-Start"
-    url: quick.html
-  - title: "Builtin commands"
-    url: builtin.html
+    url: documentation.html#QuickStart
+  - title: Documentation
+    url: documentation.html
+  - title: "Mist Language"
+    url: syntax.html
 
+# sidebar links
 sidebar:
   - title: Home
     url: /
-  - title: Introduction
-    url: intro.html
-  - title: Quick Start (ping)
-    url: quick.html
-  - title: Builtin commands
+  - title: Documentation
+    url: documentation.html
     children:
-      - title: Data
-        url: builtin.html#data
-      - title: Put
-        url: builtin.html#put
-      - title: Print
-        url: builtin.html#print
-      - title: Check
-        url: builtin.html#check
-      - title: Iterate
-        url: builtin.html#iterate
-      - title: Exec
-        url: builtin.html#exec
-      - title: SearchInText
-        url: builtin.html#searchInText
-      - title: SearchInXML
-        url: builtin.html#searchInXML
-      - title: SearchInJSON
-        url: builtin.html#searchInJSON
-      - title: CSVDump
-        url: builtin.html#csvDump
-      - title: CSVput
-        url: builtin.html#csvPut
-  - title: Command catalogs
+      - title: Introduction
+        url: documentation.html#Introduction
+      - title: Quick Start
+        url: documentation.html#QuickStart
+  - title: Mist Language
     children:
-      - title: Core catalog
-        url: https://cr0hn.github.io/mist-catalog/
-      - title: AWS catalog
-        url: https://hhurtado.github.io/mist-aws-catalog/
-  - title: Playbook catalogs
-    url: https://cr0hn.github.io/mist-playbooks/EOF
+      - title: Syntax
+        url: syntax.html
+      - title: Functions
+        url: functions.html
+      - title: Catalog
+        url: catalog.html
 EOF
+
+echo -e "Generating doc file: \n$DOCS_FILE"
+echo -e "# Mist Documentation\n\n<a id=\"Documentation\"></a>" > $DOCS_FILE
+csplit --prefix=docs --suppress-matched  README.md '/<!--split-->/' '{*}'
+grep -v "MIST LOGO" docs00 >> $DOCS_FILE
+echo -e "\n\n<a id=\"Introduction\"></a>" >> $DOCS_FILE
+cat mist/lang/README.md >> $DOCS_FILE
+echo -e "\n\n<a id=\"QuickStart\"></a>" >> $DOCS_FILE
+sed -e 's#https://raw.githubusercontent.com/BBVA/mist/master#content/assets#' docs01 >> $DOCS_FILE
+echo -e "\n\n" >> $DOCS_FILE
+cat docs02 >> $DOCS_FILE
+
+echo "$LANG_SYNTAX_FILE"
+cp -v mist/lang/README.md $LANG_SYNTAX_FILE
+
+echo "$LANG_FUNCS_FILE"
+cp -v mist/lang/BUILTIN.md $LANG_FUNCS_FILE
+
+echo "$LANG_CATALOG_FILE"
+echo "# Mist Catalog" > $LANG_CATALOG_FILE
+for item in mist/catalog/*.md
+do
+  command=$(basename $item .md)
+  echo -e "\n\n<a id=\"$item\"></a>" >> $LANG_CATALOG_FILE
+  cat $item >> $LANG_CATALOG_FILE
+done
+
+rm docs??
