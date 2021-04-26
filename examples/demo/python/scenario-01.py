19a5b5cf041673cb2b047a5fb8a2be37b56948da
german.ramos.garcia <german.ramos.garcia@bbva.com>
2021-04-26 13:38:09 +0200

    refactor(demo): scenario 1 with python and waits

--- examples/demo/python/scenario-01.py
+++ examples/demo/python/scenario-01.py
@@ -1,19 +1,33 @@
-import sys
-import asyncio
+import sys, asyncio, inspect
 from catalog import searchDomains, findOpenPorts
 
-domain = sys.argv[1]
+done = 0
 
-async def readQueueAndRun(q, f, *args):
-    while True:
-        asyncio.create_task(f(await q.get(), *args))
+async def producer(f, *args):
+    await asyncio.create_task(f(*args))
+    global done
+    done = done + 1 
+
+async def consumer(q, f, n, *args):
+    global done
+    while not q.empty() or done < n:
+        try:
+            item = q.get_nowait()
+            if inspect.iscoroutinefunction(f):
+                await f(item, *args)
+            else:
+                f(item, *args)
+        except asyncio.QueueEmpty as e:
+            await asyncio.sleep(2)
+    done = done + 1
 
 async def main():
+    tasks = []
     domainsQueue = asyncio.Queue()
     portsQueue = asyncio.Queue()
-    asyncio.create_task(searchDomains(domain, domainsQueue))
-    asyncio.create_task(readQueueAndRun(domainsQueue, findOpenPorts, "80,443", portsQueue))
-    while True:
-        print(await portsQueue.get())
+    tasks.append(asyncio.create_task(producer(searchDomains, sys.argv[1], domainsQueue)))
+    tasks.append(asyncio.create_task(consumer(domainsQueue, findOpenPorts, 1, "80,443", portsQueue)))
+    tasks.append(asyncio.create_task(consumer(portsQueue, print, 2)))
+    await asyncio.gather(*tasks)
 
 asyncio.run(main())
