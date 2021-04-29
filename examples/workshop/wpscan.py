e10457be843fdf5fbddaf6e031d5dec2c21334c3
german.ramos.garcia <german.ramos.garcia@bbva.com>
2021-04-29 16:37:41 +0200

    fix(workshop): write results as json instead of python dict

--- examples/workshop/wpscan.py
+++ examples/workshop/wpscan.py
@@ -4,7 +4,13 @@ from mist.lang.streams import streams
 async def wpscan(url, stack:list=None, commands:list=None):
     proc = await asyncio.create_subprocess_shell(f"wpscan --url {url} --detection-mode passive --format json", stdout=asyncio.subprocess.PIPE)
     stdout, _ = await proc.communicate()
-    parsedOutput = json.loads(stdout)
+    parsedOutput = json.loads(stdout)    
+    r = {
+        "resultCode": proc.returncode,
+        "result": proc.returncode == 0,
+        "consoleOutput": stdout.decode('utf-8'),
+        "wpinfo": parsedOutput
+    }
     if "targetStream" in stack[-1]:
-        await streams.send(stack[-1]["targetStream"][0], parsedOutput)
-    return parsedOutput
+        await streams.send(stack[-1]["targetStream"][0], r)
+    return r
